1c6153caf4d9fe787e2be23dcca49ba30f4fef35
kevin.davison <kevin.davison@logicdrop.com>
2016-12-21 14:18:17 -0500

    slowly adding htmlwork to angularjs project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1><div>Hello world!</div>'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'WE REMAIN';
 }
