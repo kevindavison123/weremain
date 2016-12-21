80b9fe9cdee9d5d09f3342f37bde878e72721112
kevin.davison <kevin.davison@logicdrop.com>
2016-12-21 11:34:21 -0500

    updated app comp and module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: '<h1>{{title}}</h1><div>Hello world!</div>'
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+
+const appRoutes: Routes = [
+
+];
+
 
 @NgModule({
   declarations: [
