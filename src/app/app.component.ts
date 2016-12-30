4be46dc7c146a50fc3ad5aa22cce6e86a8539fc9
kevin.davison <kevin.davison@logicdrop.com>
2016-12-30 11:57:59 -0500

    changed routing around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../css/index.css','../css/master_a-master.css','../css/site_global.css']
 })
 export class AppComponent {
   title = 'WE REMAIN';
