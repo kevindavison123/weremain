af3030e91bcd7ea7266e440e29423b4158a2f68e
Kyle Davison <kdavison@gmail.com>
2017-02-07 21:26:21 -0600

    mostly there.

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,21 @@ FROM node:latest
 
 MAINTAINER Kyle Davison <kdavison@gmail.com>
 
-RUN apt-get update
-
 # idk if this is really the best place...
 ENV APP weremainfund.org
 ENV WEBROOT /srv/www/${APP}
 
-RUN mkdir -p ${WEBROOT}
+RUN \
+  apt-get update && \
+  mkdir -p ${WEBROOT}
+
 WORKDIR ${WEBROOT}
 COPY . ${WEBROOT}
 
 #install npm
-RUN npm install -g angular-cli@latest
-RUN npm install
-RUN npm install express --save
-RUN node httpsServer.js
+RUN \
+  npm install -g angular-cli@latest && \
+  npm install
 
 EXPOSE 4200
 
--- httpsServer.js
+++ httpsServer.js
@@ -1,24 +1,40 @@
-var http       = require('http');
-var express    = require('express');
-var HTTP_PORT  = 4200;
-var HTTPS_PORT = 443;
+var http        = require('http')
+var express     = require('express')
+var app         = express()
+var fs          = require('fs')
 
-var http_app = express();
-http_app.set('port', HTTP_PORT);
 
-http_app.all('/*', function(req, res, next) {
-    if (/^http$/.test(req.protocol)) {
-        var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
-        if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
-            return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
-        } else {
-            return res.redirect("https://" + host + req.url, 301);
-        }
-    } else {
-        return next();
-    }
+var static_root = __dirname + '/';
+app.set('http_port', (process.env.PORT || 4200));
+app.set('https_port', 443);
+
+
+/*
+app.all('/*', function(req, res, next) {
+  if(/^http$/.test(req.protocol)) {
+    var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any 
+    console.log('redirect @ %s', host);
+    return res.redirect(301, "https://" + host + ":" + HTTPS_PORT + req.url);
+  } else {
+    console.log('next!');
+    return next();  
+  }
+})
+*/
+
+app.use(express.static(static_root));
+
+app.use(function(req, res, next){
+  var accept = req.accepts('html');
+  if(accept !== 'html') {
+    console.log('serve some html!');
+    return next();  
+  }
+
+  fs.createReadStream(static_root + 'index.html').pipe(res);
 });
 
-http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
-    return console.log("HTTP to HTTPS redirect app launched.");
-});
+app.listen(app.get('http_port'), function() {
+  console.log('serving on port ', app.get('http_port'));
+})
+
