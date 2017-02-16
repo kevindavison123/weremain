c95ef3f4374fb241bb7e1642f37d66faf99aba88
kevin.davison <kevin.davison@logicdrop.com>
2017-02-15 20:59:35 -0500

    added css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,18 @@ import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './index.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+    './css/site_global.css',
+    './css/blog.css',
+    './css/donate.css',
+    './css/ensuring-environmental-justice.css',
+    './css/index.css',
+    './css/master_menu.css',
+    './css/protecting-lgbtqa--rights.css',
+    './css/protecting-muslim-american-rights.css',
+    './css/protecting-our-democracy.css',
+    './css/protecting-women_s-health-and-reproductive-justice.css',
+    './css/protecting-womens-health-and-reproductive-justice.css']
 })
 export class AppComponent {
 
