2bd0b0509a45e647b0b32e3928110bd0fc538f0c
kevin.davison <kevin.davison@logicdrop.com>
2017-02-15 20:44:45 -0500

    adding website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { routing } from './app.router';
-import { effects, store, instrumentation } from './store';
-import { SharedModule } from './shared/shared.module';
-import { WeatherService } from './weather/weather.service';
 
 @NgModule({
   declarations: [
@@ -15,16 +11,10 @@ import { WeatherService } from './weather/weather.service';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
     FormsModule,
     HttpModule,
-    store,
-    effects,
-    routing,
-    instrumentation
   ],
   providers: [
-    WeatherService
   ],
   bootstrap: [
     AppComponent
